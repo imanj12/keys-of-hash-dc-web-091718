86e36a8e0bcf93aa104594e165276c50b1288e09
Iman Jawad <imanj12@gmail.com>
2018-09-10 20:51:39 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,14 @@
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    
+    array = []
+    self.each do |key, values|
+      if values == arguments
+        array << key
+      end
+    end
+    array
+  
   end
 end
